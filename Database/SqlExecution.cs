cf422aea47d2f668688a001296bfa8fa36dfa360
hamzalafsioui <hamzalafsioui@gmail.com>
2026-01-17 21:09:56 +0100

    SqlExecution.cs

---
+++ Database/SqlExecution.cs
@@ -0,0 +1,51 @@
+namespace Database;
+
+/// <summary>
+/// SQL Execution Engine - Routes queries to appropriate handlers
+/// </summary>
+public class SqlExecution
+{
+    private readonly BufferPoolManager _buffer;
+    private object? _results;
+
+    public SqlExecution(string database)
+    {
+        _buffer = new BufferPoolManager(database);
+    }
+
+    /// <summary>
+    /// Execute a parsed SQL query
+    /// </summary>
+    public void Execute(SqlParser parser)
+    {
+        _results = parser.MethodType switch
+        {
+            "SELECT" => HandleSelect(parser),
+            "CREATE" => HandleCreateTable(parser),
+            "INSERT" => HandleInsert(parser),
+            _ => "Error: Unknown command"
+        };
+    }
+
+    private string HandleInsert(SqlParser parser)
+    {
+        _buffer.InsertRow(parser);
+        return "OK: New Row Has Been Inserted !";
+    }
+
+    private string HandleCreateTable(SqlParser parser)
+    {
+        _buffer.CreateTable(parser);
+        return "OK: New Table Created !";
+    }
+
+    private List<Dictionary<string, object>> HandleSelect(SqlParser parser)
+    {
+        return _buffer.SelectRows(parser);
+    }
+
+    /// <summary>
+    /// Get execution results
+    /// </summary>
+    public object? Results => _results;
+}
